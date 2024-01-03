11d2594118ef99022f35c3708dc85caee28962b7
Artem <goldtree3000@gmail.com>
2024-01-03 19:49:02 +0300

    feat: solve getHexRGBValues task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -462,8 +462,12 @@ function getIndicesOfOddNumbers(numbers) {
  *    getHexRGBValues([ 0, 255, 16777215]) => [ '#000000', '#0000FF', '#FFFFFF' ]
  *    getHexRGBValues([]) => []
  */
-function getHexRGBValues(/* arr */) {
-  throw new Error('Not implemented');
+function getHexRGBValues(arr) {
+  const result = [];
+  arr.map((el) =>
+    result.push(`#${`000000${el.toString(16)}`.slice(-6).toUpperCase()}`)
+  );
+  return result;
 }
 
 /**
